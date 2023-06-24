7fa70578330945f62e7c307beed91f48bc715809
forteller <michu.roztocz@gmail.com>
2023-06-24 09:44:08 +0200

    Added backup.sh script with its description.

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ This script has been made out of frustration, but it basically does what "Instal
 As an omv user since omv3 this makes me mad. With omv3->omv4 transition omv creators enforced the use of Docker for basic NAS functionality by not porting over many previously existing omv plugins with intention to use docker instead. Back then, they provided crappy GUI for maintaining the docker environment. With omv4->omv5 they removed this plugin and forced everyone to use portainer. Now mid-omv6 existence, basic functionality is changed and after some update, whole part of the system has been removed and they want me to use their plugin again. And of course there is some Portainer->OMV tutorial to follow and new GUI to learn.
 
 This is just a temporary solution. I intend to move away from omv, because this is not the first time they pull such thing off.
+
+## backup.sh
+Simple backup script I have written for my personal needs.
+
+Syntax: ./backup.sh <source_dir> <target_dir>
+
+It creates .tar archive from directory given as a first argument and places it in target directory specified in second argument. Then, it deletes any files in target directory that are older than 5 days.
+
+!!! Be cautious when specifying target directory !!!
---
+++ backup.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#
+# forteller 27.02.2022
+
+# Check if directories exist
+if [ ! -d "/$1" ]; then
+  echo "Incorrect source specified!"
+  exit 1;
+fi
+
+if [ ! -d "$2" ]; then
+  echo "Incorrect target specified!"
+  exit 2;
+fi
+
+#Variables definitions
+line="\n\n------------------------------------------\n\n"
+source_dir="$1"
+target_dir="$2"
+source_path_first_char=`echo "$source_dir" | cut -c1`
+source_path_last_char=`echo "$source_dir" | rev | cut -c1`
+target_path_last_char=`echo "$target_dir" | rev | cut -c1`
+
+#Ensure that filename_prefix extctraction works ok / strip last slash
+if [[ "$source_path_last_char" == "/" ]]; then
+  printf "Removing last slash from source directory\n"
+  source_dir=`echo "$source_dir" | rev | cut -c2- | rev`
+fi
+
+#Ensure that target dir works ok
+if [[ "$target_path_last_char" != "/" ]]; then
+  printf "Adding slash to target directory\n"
+  target_dir="${target_dir}/"
+fi
+
+filename_prefix=`echo "$source_dir" | rev | cut -d"/" -f1 | rev`
+backup_filename="${filename_prefix}_`date +%d`_`date +%m`_`date +%Y`.tar"
+
+if [[ -f $target_dir$backup_filename ]]; then
+  printf "Target file exists! Script will now quit\n";
+  exit 3;
+fi
+
+#Actual logic
+printf "Target file: $target_dir$backup_filename\n";
+printf "Source path: $source_dir";
+printf $line
+
+date "+%d/%m/%Y %T";
+printf "Listing target directory contents before doing anything:\n";
+ls_before=`ls -lah "$target_dir"`;
+df_before=`df -h $target_dir | awk 'NR==2{print$4}'`;
+printf "$ls_before";
+printf "\nFree space: $df_before";
+printf $line;
+
+#Removing first slash for tar to stop complaining
+if [[ "$source_path_first_char" == "/" ]]; then
+  source_dir=`echo "$source_dir" | cut -c2-`
+fi
+
+date "+%d/%m/%Y %T";
+printf "Now creating backup archive"
+tar cf "$target_dir""$backup_filename" -C / "$source_dir"
+printf $line
+
+date "+%d/%m/%Y %T";
+printf "Now deleting files older than 5 days"
+find $target_dir -type f -mtime +5 -exec rm {} \;
+printf $line
+
+date "+%d/%m/%Y %T";
+printf "Listing target directory contents:\n"
+ls_after=`ls -lah "$target_dir"`;
+df_after=`df -h $target_dir | awk 'NR==2{print$4}'`;
+printf "$ls_after";
+printf "\nFree space: $df_after";
+printf $line;
+
+date "+%d/%m/%Y %T";
+printf "Direcotry listing differences:\n"
+diff -y --suppress-common-lines <(printf "Before\n$ls_before" ) <(printf "After\n$ls_after")
+printf "\n"
+exit 0;
