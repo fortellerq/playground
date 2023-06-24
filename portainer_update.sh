035a137633f59861617bb3537f5768ecf562e7df
forteller <michu.roztocz@gmail.com>
2023-06-24 09:51:43 +0200

    Slight refactor. No functional changes.

--- portainer_update.sh
+++ portainer_update.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+#
+# forteller 24.06.2023
+
 printf "Stopping: "
 docker stop portainer
+
 printf "Removing: "
 docker rm portainer
+
+printf "Pulling latest portainer image: "
 docker pull portainer/portainer-ce:latest
+
+printf "Starting new portainer container: "
 docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
