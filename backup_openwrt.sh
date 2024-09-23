ec1442361049620ac10465ce204186b9982b9333
Michał R <michu.roztocz@gmail.com>
2024-09-23 22:07:20 +0200

    Update backup_openwrt.sh
    Added -k option so list of installed packages is preserved as well

--- backup_openwrt.sh
+++ backup_openwrt.sh
@@ -2,6 +2,7 @@
 #
 # forteller 23.09.2024
 #
+# 24/09/23 - v0.2 - added -q and -k options
 # 24/09/23 - v0.1 - initial version
 #
 # Usage: ./backup_openwrt.sh [IP] [port] [keyfile_path] [target_path]
@@ -17,7 +18,7 @@ target_path="$4"
 $target_dir
 
 echo "ssh"
-ssh $ip -p $port -i $key_path 'mkdir -p /root/backup && sysupgrade -q -b /root/backup/backup-${HOSTNAME}-$(date +%F).tar.gz'
+ssh $ip -p $port -i $key_path 'mkdir -p /root/backup && sysupgrade -q -k -b /root/backup/backup-${HOSTNAME}-$(date +%F).tar.gz'
 echo "scp"
 scp -P $port -i $key_path root@$ip:/root/backup/* $target_path
 exit 0;
