a6feb908cd79087161033fe7328228faa200ff55
Michał Roztoczyński <michu.roztocz@gmail.com>
2024-09-24 19:40:54 +0200

    backup_openwrt.sh update

--- backup_openwrt.sh
+++ backup_openwrt.sh
@@ -2,23 +2,42 @@
 #
 # forteller 23.09.2024
 #
+# 24/09/24 - v0.3 - code cleanup, requirement to redirect STDOUT and STDERR, sending mail when fail, target cleanup
 # 24/09/23 - v0.2 - added -q and -k options
 # 24/09/23 - v0.1 - initial version
 #
-# Usage: ./backup_openwrt.sh [IP] [port] [keyfile_path] [target_path]
+# Usage: ./backup_openwrt.sh [IP] [PORT] [KEY_PATH] [TARGET_PATH] 1>[STDOUT] 2>[STDERR]
 
-#Variables definitions
-line="\n------------------------------------------------------------------------------\n"
-ip="$1"
-port="$2"
-key_path="$3"
-target_path="$4"
+# variables definitions
+IP="$1"
+PORT="$2"
+KEY_PATH="$3"
+TARGET_PATH="$4"
+STD_OUT=`readlink /proc/$$/fd/1`
+STD_ERR=`readlink /proc/$$/fd/2`
+MAILTO=root
+# ----------------------------------------------------------------------------------------------
 
-#Actual logic
-$target_dir
+# check if STD_OUT / STD_ERR redirected
+if [[ "$STD_OUT" == *"/dev/pts"* ]] || [[ "$STD_ERR" == *"/dev/pts"* ]]
+ then
+  ERRORTXT='this script has to have STDOUT and STDERR redirected to file'
+  echo $ERRORTXT >&2
+  echo $ERRORTXT | mail -s "Error occured when performing openwrt backup" root
+  exit 11
+fi
 
-echo "ssh"
-ssh $ip -p $port -i $key_path 'mkdir -p /root/backup && sysupgrade -q -k -b /root/backup/backup-${HOSTNAME}-$(date +%F).tar.gz'
-echo "scp"
-scp -P $port -i $key_path root@$ip:/root/backup/* $target_path
-exit 0;
+# actual logic
+ssh $IP -p $PORT -i $KEY_PATH 'mkdir -p /root/backup && sysupgrade -q -k -b /root/backup/backup-${HOSTNAME}-$(date +%F).tar.gz'
+scp -P $PORT -i $KEY_PATH root@$IP:/root/backup/* $TARGET_PATH
+ssh $IP -p $PORT -i $KEY_PATH 'rm -rf /root/backup/*'
+
+# send an email if there are errors
+if [[ -s $STD_ERR ]]
+ then
+  echo "errors occured during script execution" >&2
+  cat $STD_ERR | mail -s "Error occured when performing openwrt backup" $MAILTO
+  exit 10
+fi
+
+exit 0
